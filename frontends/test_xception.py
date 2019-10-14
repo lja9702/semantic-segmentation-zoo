e71178257159ebe606033800001b8ef9f98141fd
lja9702 <lja9702@naver.com>
2019-10-14 23:39:02 +0900

    summary test

---
+++ frontends/test_xception.py
@@ -0,0 +1,35 @@
+import tensorflow as tf
+from xception import Xception
+tf.set_random_seed(777)
+from PIL import Image
+import numpy as np
+from keras.applications.inception_v3 import preprocess_input #InceptionV3, Xception, InceptionResNetV2
+#from keras.applications.imagenet_utils import preprocess_input #others
+
+file_path = tf.keras.utils.get_file('ImageNetLabels.txt','https://storage.googleapis.com/download.tensorflow.org/data/ImageNetLabels.txt')
+labels = open(file_path).read().splitlines()
+print(len(labels)) #1001
+del labels[0]
+print(len(labels)) #1000
+
+#model = Xception() #Downloading data from https://github.com/fchollet/deep-learning-models/releases/download/v0.4/xception_weights_tf_dim_ordering_tf_kernels.h5
+
+##########모델 예측
+
+file_path = tf.keras.utils.get_file('cock.png', 'http://www.farmersonlinemarket.co/wp-content/uploads/2018/01/cock.png')
+image = Image.open(file_path)
+width = 299
+height = 299
+image = image.resize((width, height))
+image = np.array(image)
+print(image.shape) #(224, 224, 3)
+x_test = [image]
+x_test =  np.array(x_test)
+#x_test = x_test / 255
+x_test = preprocess_input(x_test)
+
+y_predict = Xception.predict(x_test, 1000)
+print(y_predict) #[[0.20842288 0.41051054 0.38106653]]
+print(y_predict.argmax(axis=1)) #[1]
+print(y_predict.argmax(axis=1)[0]) #1
+print(labels[y_predict.argmax(axis=1)[0]]) #B
--- frontends/xception.py
+++ frontends/xception.py
@@ -2,69 +2,76 @@ import tensorflow as tf
 import tensorflow.keras
 
 from tensorflow.keras import models, layers
-from tensorflow.keras.layers import Dense, Activation, Conv2D, MaxPooling2D, SeparableConv2D, BatchNormalization, Input, GlobalAveragePooling2D
+from tensorflow.keras.layers import Add, Dense, Activation, Conv2D, MaxPooling2D, SeparableConv2D, BatchNormalization, Input, GlobalAveragePooling2D
 
-def conv2d_block(x, filters, kernel_size, padding = 'same', strides = 1, activation = None):
-    x = Conv2D(filters, kernel_size, padding = padding, strides = strides)(x)
+
+def conv2d_block(x, filters, kernel_size, padding='same', strides=(1, 1), activation=None):
+    x = Conv2D(filters, kernel_size, padding=padding, strides=strides)(x)
     x = BatchNormalization()(x)
     if activation:
         x = Activation(activation)(x)
 
     return x
 
-def separableconv2d_block(x, filters, kernel_size, padding = 'same', strides = 1, activation = None):
-    x = SeparableConv2D(filters, kernel_size, padding = padding, strides = strides, depth_multiplier = 1)
+def separableconv2d_block(x, filters, kernel_size, padding='same', strides=(1, 1), activation=None):
+    x = SeparableConv2D(filters, kernel_size, padding=padding, strides=strides, depth_multiplier=1)(x)
     x = BatchNormalization()(x)
     if activation:
         x = Activation(activation)(x)
 
     return x
 
-def xception(inputs, num_classes, activation='softmax'):
+def Xception(input_shape=(299, 299, 3), classes=1000, activation='softmax'):
 
+    input = Input(shape = input_shape)
     ######## entry flow ########
-    x = conv2d_block(input, 32, 3, strides = 2, activation = 'relu')
-    x = conv2d_block(x, 64, 3, activation = 'relu')
+    x = conv2d_block(input, 32, (3, 3), strides=(2, 2), activation='relu')
+    x = conv2d_block(x, 64, (3, 3), activation='relu')
     filters = [128, 256, 728];
-    
+
     for filter in filters:
-        residual_network = conv2d_block(x, filter, 1, strides = 2)
+        residual_network = conv2d_block(x, filter, (1, 1), strides=(2, 2))
 
         if filter != filters[0]:
-            x = Activation(activation = 'relu')(x)
+            x = Activation(activation='relu')(x)
+        x = separableconv2d_block(x, filter, (3, 3), activation='relu')
+        x = separableconv2d_block(x, filter, (3, 3))
 
-        x = separableconv2d_block(x, filter, 3, activation = 'relu')
-        x = separableconv2d_block(x, filter, 3)
-
-        x = MaxPooling2D(3, strides = 2, padding = 'same')(x)
+        x = MaxPooling2D((3, 3), strides=(2, 2), padding='same')(x)
 
         x = Add()([x, residual_network])
 
     ######## middle flow ########
     for i in range(8):
+        residual_network = x
+
         x = Activation(activation = 'relu')(x)
-        x = separableconv2d_block(x, 728, 3, activation = 'relu')
-        x = separableconv2d_block(x, 728, 3, activation = 'relu')
-        x = separableconv2d_block(x, 728, 3)
+        x = separableconv2d_block(x, 728, (3, 3), activation='relu')
+        x = separableconv2d_block(x, 728, (3, 3), activation='relu')
+        x = separableconv2d_block(x, 728, (3, 3))
 
         x = Add()([x, residual_network])
 
     ######## exit flow ########
-    residual_network = conv2d_block(x, filters, 1, strides = 2)
+    residual_network = conv2d_block(x, 1024, (1, 1), strides=(2, 2))
 
-    x = Activation(activation = 'relu')(x)
-    x = separableconv2d_block(x, 728, 3, activation = 'relu')
-    x = separableconv2d_block(x, 1024, 3)
-    x = MaxPooling2D(3, strides = 2, padding = 'same')(x)
+    x = Activation(activation='relu')(x)
+    x = separableconv2d_block(x, 728, (3, 3), activation='relu')
+    x = separableconv2d_block(x, 1024, (3, 3))
+    x = MaxPooling2D((3, 3), strides=(2, 2), padding='same')(x)
 
     x = Add()([x, residual_network])
 
-    x = separableconv2d_block(x, 1536, 3, activation = 'relu')
-    x = separableconv2d_block(x, 2048, 3, activation = 'relu')
+    x = separableconv2d_block(x, 1536, (3, 3), activation='relu')
+    x = separableconv2d_block(x, 2048, (3, 3), activation='relu')
 
     x = GlobalAveragePooling2D()(x)
 
-    output = Dense(num_classes, activation = activation)(x)
-    model = Model(inputs, output, name = 'Xception')
+    output = Dense(classes, activation=activation)(x)
+    model = models.Model(input, output, name='Xception')
 
     return model
+
+
+xception = Xception()
+xception.summary()
