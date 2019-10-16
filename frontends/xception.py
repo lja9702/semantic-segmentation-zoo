92324ffa246b9ff905c4f59d6667d21699cc8ba7
lja9702 <lja9702@naver.com>
2019-10-17 01:27:20 +0900

    Modify Xception

--- frontends/xception.py
+++ frontends/xception.py
@@ -1,11 +1,12 @@
+from __future__ import absolute_import, division, print_function
 import tensorflow as tf
 import tensorflow.keras
-
 from tensorflow.keras import models, layers
-from tensorflow.keras.layers import Add, Dense, Activation, Conv2D, MaxPooling2D, SeparableConv2D, BatchNormalization, Input, GlobalAveragePooling2D
+from tensorflow.keras.layers import Add, Dense, Activation, Input
+from tensorflow.keras.layers import  Conv2D, MaxPooling2D, SeparableConv2D, BatchNormalization, GlobalAveragePooling2D
 
 def conv2d_block(x, filters, kernel_size, padding='same', strides=(1, 1), activation=None):
-    x = Conv2D(filters, kernel_size, padding=padding, strides=strides)(x)
+    x = Conv2D(filters, kernel_size, padding=padding, strides=strides, use_bias=False)(x)
     x = BatchNormalization()(x)
     if activation:
         x = Activation(activation)(x)
@@ -13,19 +14,22 @@ def conv2d_block(x, filters, kernel_size, padding='same', strides=(1, 1), activa
     return x
 
 def separableconv2d_block(x, filters, kernel_size, padding='same', strides=(1, 1), activation=None):
-    x = SeparableConv2D(filters, kernel_size, padding=padding, strides=strides, depth_multiplier=1)(x)
+    x = SeparableConv2D(filters, kernel_size, padding=padding, strides=strides, use_bias=False)(x)
     x = BatchNormalization()(x)
     if activation:
         x = Activation(activation)(x)
 
     return x
 
-def Xception(input, classes=1000, activation='softmax'):
+def Xception(input=None, num_classes=1000, is_training=True, activation='softmax'):
+
+    if input is None:
+        input = Input(shape=(299, 299, 3))
 
     ######## entry flow ########
-    x = conv2d_block(input, 32, (3, 3), strides=(2, 2), activation='relu')
-    x = conv2d_block(x, 64, (3, 3), activation='relu')
-    filters = [128, 256, 728];
+    x = conv2d_block(input, 32, (3, 3), strides=(2, 2), padding='valid', activation='relu')
+    x = conv2d_block(x, 64, (3, 3), padding='valid', activation='relu')
+    filters = [128, 256, 728]
 
     for filter in filters:
         residual_network = conv2d_block(x, filter, (1, 1), strides=(2, 2))
@@ -65,7 +69,8 @@ def Xception(input, classes=1000, activation='softmax'):
 
     x = GlobalAveragePooling2D()(x)
 
-    output = Dense(classes, activation=activation)(x)
+    output = Dense(num_classes, activation=activation)(x)
+
     model = models.Model(input, output, name='Xception')
 
     return model
